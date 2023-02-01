59a04a9b8800dbd9f245a14fa9e3ea4deae37256
KonstaLaineinen <konsta.laineinen@windowslive.com>
2023-02-01 21:08:31 +0200

    .

---
+++ EX2/footer.php
@@ -0,0 +1,37 @@
+footer> 
+                <div class="footer-widget">
+                  <div class="container">
+                      <div class="row">
+                          <div class="row justify-content-center">
+                          <div class="col-sm-6">
+                              <div class="single-widget">
+                                  <h2>About Cecile</h2>
+                                  <ul class="nav nav-pills nav-stacked">
+                                      <li><a href="404.html">About us</a></li>
+                                      <li><a href="404.html">Work with us</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                          <div class="col-sm-6">
+                              <div class="single-widget">
+                                  <h2>Information</h2>
+                                  <ul class="nav nav-pills nav-stacked">
+                                      <li><a href="404.html">Track order</a></li>
+                                      <li><a href="404.html">Delivery and Returns</a></li>
+                                      
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                </div>
+          
+                  <div class="footer-bottom">
+                    <div class="container">
+                        <div class="row">
+                            <p class="pull-left">Copyright © 2023 Cecile. All rights reserved.</p>
+                            <p> <img src="images/logo.png"  alt="logo2"></p>
+                        </div>
+                    </div>
+                  </div>
+              </footer>
