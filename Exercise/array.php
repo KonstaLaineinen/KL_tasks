a495eab51b7f6c9ab27bb43c30b114f3dc8007be
KonstaLaineinen <konsta.laineinen@windowslive.com>
2023-02-09 19:33:07 +0200

    comments to answer questions

--- Exercise/array.php
+++ Exercise/array.php
@@ -153,6 +153,57 @@ echo $temp_array[$i].", ";
 ?>
 
 
+<!-- Write comments to explain the following code (when asked):
+*/
+echo "<hr><h2> Calculation average temperature: </h2>";
+$month_temp = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
+68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+
+
+// what is explode and what does the below code do? : 
+
+Split a string by a string
+
+
+$temp_array = explode(',', $month_temp);
+$tot_temp = 0;
+
+
+
+// What is count?
+
+Counts all elements
+
+$temp_array_length = count($temp_array);
+foreach($temp_array as $temp)
+{
+ $tot_temp += $temp;
+}
+ $avg_high_temp = $tot_temp/$temp_array_length;
+ echo "Average Temperature is : ".$avg_high_temp."
+"; 
+
+
+
+// what does sort do?
+
+Sorts an array in an ascending order
+
+sort($temp_array);
+echo "<br> List of five lowest temperatures :";
+for ($i=0; $i< 5; $i++)
+{ 
+echo $temp_array[$i].", ";
+}
+echo "<br>List of five highest temperatures :";
+// explain the following loop
+for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
+{
+echo $temp_array[$i].", ";
+}
+?>
+ -->
+
 
 
 
