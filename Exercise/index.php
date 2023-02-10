08884b33b090c0f1aa684ef979680bad9b282655
KonstaLaineinen <konsta.laineinen@windowslive.com>
2023-02-10 14:22:35 +0200

    made the page almost correct looking

--- Exercise/index.php
+++ Exercise/index.php
@@ -1,13 +1,91 @@
 <?php include "header.php" ?>
 
-<h1> Exercise 2 </h1>
+<link rel="stylesheet" href="styles.css">
 
-<h2>Create universal header, footer, or sidebar as per your layout that you have done in the previous course. The same layout will be used for all individual tasks. 
-You are required to have a menu bar that includes links to all in-class tasks. Create a link to ex1.php </h2>
+<div class="container">
 
-<p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, quae aut aliquam ea adipisci quisquam quam. Nulla ducimus, porro debitis perspiciatis nam incidunt ipsum rerum impedit sint dolores quos facilis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate laborum eligendi ab. Obcaecati laudantium eligendi at neque. Quod ad quaerat labore velit ratione ex, nemo, provident, at tenetur laudantium soluta!
-</p>
+
+      
+<div class="p-5 text-center bg-image rounded-3" style="background-image: url('images/banner2.jpg');height: 600px;  background-repeat: no-repeat;">
+<div class="mask" style="background-color: #ffe8e0d5">
+<div class="d-flex justify-content-center align-items-center h-100">
+  <div class="text-white">
+    <h1 class="mb-3 " style="padding-top: 16px; padding-bottom: 16px;">Wear clothes that make you be yourself</h1>
+
+  </div>
+
+  
+</div>
+</div>
+</div>
+
+  
+
+
+
+
+
+
+
+
+
+    <div class="popular" style=" padding-top: 78px; padding-bottom: 46px;">
+      <p>Popular Products</p>
+    </div>
+
+
+  
+
+
+
+
+
+  
+
+
+  <div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-4" style="float: left;  margin-left: 100px;"  >
+        <div class="product-grid">
+            <div class="product-image">
+
+                
+
+                <a href="405.html" class="image">
+                    <img src="images/model2.2.png" alt="Hoodie">
+
+                </a>
+                <a href="404.html" class="add-to-cart">OUT OF STOCK</a>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="405.html">Hoodie</a></h3>
+                <div class="price">60.00€</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-sm-4" style="float: right;  margin-right: 100px;" >
+      <div class="product-grid">
+          <div class="product-image">
+              <a href="405.html" class="image">
+
+                  
+
+                  <img src="images/model4.3.png" alt="Sweater">
+
+              </a>
+              
+              <a href="404.html" class="add-to-cart">OUT OF STOCK</a>
+          </div>
+          <div class="product-content">
+              <h3 class="title"><a href="405.html">Sweater</a></h3>
+              <div class="price">49.00€ <span>70.00€</span></div>
+              
+          </div>
+      </div>
+  </div>
+
+</div>
+
 
 
 
